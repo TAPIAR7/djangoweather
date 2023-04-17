cec35bf6746f2aab0422392980e9896d4faf1a5d
Ricardo Tapia Venegas <rtapiatec@gmail.com>
2023-04-16 18:28:08 -0600

    Changing view.py in lookup application

--- lookup/views.py
+++ lookup/views.py
@@ -15,27 +15,27 @@ def home(request):
 		except Exception as e:
 			api = "Error..."
 			
-
-		#if api != "Error...":
 		try:
 			if api[0]['Category']['Name'] == "Good":
-			  	category_description = "(0 - 50)"
-			  	category_color = "good"
+				category_description = "(0 - 50)"
+				category_color = "good"
 			elif api[0]['Category']['Name'] == "Moderate":
-			  	category_description = "(51 - 100)"
-			  	category_color = "moderate"
+				category_description = "(51 - 100)"
+				category_color = "moderate"
+				
 			elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
 				category_description = "(101 - 150)"
 				category_color = "usg"
 			elif api[0]['Category']['Name'] == "Unhealthy":
-			  	category_description = "(151 - 200)"
-			  	category_color = "unhealthy"
+				category_description = "(151 - 200)"
+				category_color = "unhealthy"
 			elif api[0]['Category']['Name'] == "Very Unhealthy":
-			  	category_description = "(201 - 300)"
-			  	category_color = "veryunhealthy"
+				category_description = "(201 - 300)"
+				category_color = "veryunhealthy"
 			elif api[0]['Category']['Name'] == "Hazardous":
-			  	category_description = "(301 - 500)"
-			  	category_color = "hazardous"
+				category_description = "(301 - 500)"
+				category_color = "hazardous"
+			
 		except Exception as e:
 			category_description = "No data to show,  incorrect zipcode"
 			category_color = "No data to show, incorrect zipcode"
@@ -58,23 +58,23 @@ def home(request):
 		
 		try:
 			if api[0]['Category']['Name'] == "Good":
-			  	category_description = "(0 - 50)"
-			  	category_color = "good"
+				category_description = "(0 - 50)"
+				category_color = "good"
 			elif api[0]['Category']['Name'] == "Moderate":
-			  	category_description = "(51 - 100)"
-			  	category_color = "moderate"
+				category_description = "(51 - 100)"
+				category_color = "moderate"
 			elif api[0]['Category']['Name'] == "Unhealthy for Sensitive Groups":
 				category_description = "(101 - 150)"
 				category_color = "usg"
 			elif api[0]['Category']['Name'] == "Unhealthy":
-			  	category_description = "(151 - 200)"
-			  	category_color = "unhealthy"
+				category_description = "(151 - 200)"
+				category_color = "unhealthy"
 			elif api[0]['Category']['Name'] == "Very Unhealthy":
-			  	category_description = "(201 - 300)"
-			  	category_color = "veryunhealthy"
+				category_description = "(201 - 300)"
+				category_color = "veryunhealthy"
 			elif api[0]['Category']['Name'] == "Hazardous":
-			  	category_description = "(301 - 500)"
-			  	category_color = "hazardous"
+				category_description = "(301 - 500)"
+				category_color = "hazardous"
 		except Exception as e:
 			category_description = "No data to show,  incorrect zipcode"
 			category_color = "No data to show, incorrect zipcode"
